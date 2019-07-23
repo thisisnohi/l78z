9e89c854ad65a3718c0dc3dddb58c6b81c0d1f63
nohi <thisisnohi@163.com>
2019-07-23 14:27:55 +0800

    20190723

--- spring-boot-1-5/src/main/java/nohi/think/web/HelloContoller.java
+++ spring-boot-1-5/src/main/java/nohi/think/web/HelloContoller.java
@@ -1,5 +1,8 @@
 package nohi.think.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,4 +19,13 @@ public class HelloContoller {
 		System.out.println("HelloContoller name:" + name);
 		return "hello " + name;
 	}
+
+  @RequestMapping(value = "/json/{name}" )
+  public Object respJson(@PathVariable(value = "name")String name){
+	System.out.println("HelloContoller name:" + name);
+	Map<String, String> resp = new HashMap<String, String>();
+	resp.put( "OUT_CODE", name );
+	resp.put( "OUT_MESG", "这是返回码，测试中文" );
+	return resp;
+  }
 }
--- spring-boot-1-5/src/main/resources/db/data.sql
+++ spring-boot-1-5/src/main/resources/db/data.sql
@@ -1,3 +1,3 @@
-insert into T_USER (U_ID,ADDRESS,AGE,NAME) values (1,'张三',18,'中国北京');
-insert into T_USER (U_ID,ADDRESS,AGE,NAME) values (2,'李四',19,'中国上海');
-insert into T_USER (U_ID,ADDRESS,AGE,NAME) values (3,'王五',20,'中国南京');
+insert into T_USER (U_ID,NAME,AGE,ADDRESS) values (1,'张三',18,'中国北京');
+insert into T_USER (U_ID,NAME,AGE,ADDRESS) values (2,'李四',19,'中国上海');
+insert into T_USER (U_ID,NAME,AGE,ADDRESS) values (3,'王五',20,'中国南京');
